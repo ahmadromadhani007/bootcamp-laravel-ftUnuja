c286e0673eaa7927800cf65c73aefc1d009bbdaf
Armdhann <dan10kiy@gmail.com>
2023-05-26 14:40:27 +0700

    nwe

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -1,16 +1,18 @@
-<script>var hostUrl = "assets/";</script>
-	<!--begin::Javascript-->
-	<!--begin::Global Javascript Bundle(used by all pages)-->
-	<script src="{{ url('assets/metronic/js/plugins.bundle.js') }}"></script>
-	<script src="{{ url('assets/metronic/js/scripts.bundle.js') }}"></script>
-	<!--end::Global Javascript Bundle-->
-	<!--begin::Page Vendors Javascript(used by this page)-->
-	{{-- <script src="assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
-	<!--end::Page Vendors Javascript-->
-	<!--begin::Page Custom Javascript(used by this page)-->
-	<script src="assets/js/custom/widgets.js"></script>
-	<script src="assets/js/custom/apps/chat/chat.js"></script>
-	<script src="assets/js/custom/modals/create-app.js"></script>
-	<script src="assets/js/custom/modals/upgrade-plan.js"></script> --}}
-	<!--end::Page Custom Javascript-->
-	<!--end::Javascript-->
+<script>
+    var hostUrl = "assets/";
+</script>
+<!--begin::Javascript-->
+<!--begin::Global Javascript Bundle(used by all pages)-->
+<script src="{{ asset('metronic/js/plugins.bundle.js') }}"></script>
+<script src="{{ asset('metronic/js/scripts.bundle.js') }}"></script>
+<!--end::Global Javascript Bundle-->
+{{-- <!--begin::Page Vendors Javascript(used by this page)-->
+<script src="assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
+<!--end::Page Vendors Javascript-->
+<!--begin::Page Custom Javascript(used by this page)-->
+<script src="assets/js/custom/widgets.js"></script>
+<script src="assets/js/custom/apps/chat/chat.js"></script>
+<script src="assets/js/custom/modals/create-app.js"></script>
+<script src="assets/js/custom/modals/upgrade-plan.js"></script> --}}
+<!--end::Page Custom Javascript-->
+<!--end::Javascript-->
--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -1,12 +1,12 @@
-	<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-	<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
-	<!--begin::Fonts-->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-	<!--end::Fonts-->
-	<!--begin::Page Vendor Stylesheets(used by this page)-->
-	{{-- <link href="assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" /> --}}
-	<!--end::Page Vendor Stylesheets-->
-	<!--begin::Global Stylesheets Bundle(used by all pages)-->
-	<link href="{{ url('assets/metronic/css/plugins.bundle.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{ url('assets/metronic/css/style.bundle.css')}}" rel="stylesheet" type="text/css" />
-	<!--end::Global Stylesheets Bundle-->
+{{-- <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+<!--begin::Fonts-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+<!--end::Fonts-->
+<!--begin::Page Vendor Stylesheets(used by this page)-->
+<link href="assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" /> --}}
+<!--end::Page Vendor Stylesheets-->
+<!--begin::Global Stylesheets Bundle(used by all pages)-->
+<link href="{{ asset('metronic/css/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('metronic/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
+<!--end::Global Stylesheets Bundle-->
